426d822584756b8c4e1deafc1b5206ffc1baff19
Asim Aslam <asim@aslam.me>
2018-11-11 20:18:04 +0000

    lock comment

--- lock/lock.go
+++ lock/lock.go
@@ -7,7 +7,9 @@ import (
 
 // Lock is a distributed locking interfac
 type Lock interface {
+	// Acquire a lock with given id
 	Acquire(id string, opts ...AcquireOption) error
+	// Release the lock with given id
 	Release(id string) error
 }
 
