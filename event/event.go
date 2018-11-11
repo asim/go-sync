eac77d9f9e74472944620cdb4d7d1e35b13b3c43
Asim Aslam <asim@aslam.me>
2018-11-11 20:10:49 +0000

    event interface

---
+++ event/event.go
@@ -0,0 +1,23 @@
+// Package event provides a distributed log interface
+package event
+
+// Event provides a distributed log interface
+type Event interface {
+	Log(name string) (Log, error)
+}
+
+type Log interface {
+	// Close the log handle
+	Close() error
+	// Read will read the next record
+	Read() (*Record, error)
+	// Go to an offset
+	Seek(offset int64) error
+	// Write an event to the log
+	Write(Record) error
+}
+
+type Record struct {
+	Metadata map[string]interface{}
+	Data     []byte
+}
