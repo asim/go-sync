498cd362fa9522f84556ccca3b298f494190b1d4
Asim Aslam <asim@aslam.me>
2018-12-20 11:40:06 +0000

    add redis lock

---
+++ lock/redis/redis.go
@@ -0,0 +1,93 @@
+// Package redis is a redis implemenation of lock
+package redis
+
+import (
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/micro/go-sync/lock"
+	"gopkg.in/redsync.v1"
+)
+
+type redisLock struct {
+	sync.Mutex
+
+	locks map[string]*redsync.Mutex
+	opts  lock.Options
+	c     *redsync.Redsync
+}
+
+func (r *redisLock) Acquire(id string, opts ...lock.AcquireOption) error {
+	var options lock.AcquireOptions
+	for _, o := range opts {
+		o(&options)
+	}
+
+	var ropts []redsync.Option
+
+	if options.Wait > time.Duration(0) {
+		ropts = append(ropts, redsync.SetRetryDelay(options.Wait))
+		ropts = append(ropts, redsync.SetTries(1))
+	}
+
+	if options.TTL <= time.Duration(0) {
+		ropts = append(ropts, redsync.SetExpiry(options.TTL))
+	}
+
+	m := r.c.NewMutex(id, ropts...)
+	err := m.Lock()
+	if err != nil {
+		return err
+	}
+
+	r.Lock()
+	r.locks[id] = m
+	r.Unlock()
+
+	return nil
+}
+
+func (r *redisLock) Release(id string) error {
+	r.Lock()
+	defer r.Unlock()
+	m, ok := r.locks[id]
+	if !ok {
+		return errors.New("lock not found")
+	}
+
+	err := m.Unlock()
+	delete(r.locks, id)
+	if err != false {
+		return errors.New("lock not unlocked")
+	}
+
+	return nil
+}
+
+func (r *redisLock) String() string {
+	return "redis"
+}
+
+func NewLock(opts ...lock.Option) lock.Lock {
+	var options lock.Options
+	for _, o := range opts {
+		o(&options)
+	}
+
+	nodes := options.Nodes
+
+	if len(nodes) == 0 {
+		nodes = []string{"127.0.0.1:6379"}
+	}
+
+	rpool := redsync.New([]redsync.Pool{&pool{
+		addrs: nodes,
+	}})
+
+	return &redisLock{
+		locks: make(map[string]*redsync.Mutex),
+		opts:  options,
+		c: rpool,
+	}
+}
