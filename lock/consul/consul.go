28a66c016826f42586f407756c02acb5d125e7ba
Tran Duc Loc <loc.tran.duc@nextop.asia>
2019-03-25 17:36:46 +0700

    Fix 'missing port' case when initialize a NewLock of consul

--- lock/consul/consul.go
+++ lock/consul/consul.go
@@ -88,7 +88,7 @@ func NewLock(opts ...lock.Option) lock.Lock {
 		addr, port, err := net.SplitHostPort(options.Nodes[0])
 		if ae, ok := err.(*net.AddrError); ok && ae.Err == "missing port in address" {
 			port = "8500"
-			config.Address = fmt.Sprintf("%s:%s", addr, port)
+			config.Address = fmt.Sprintf("%s:%s", ae.Addr, port)
 		} else if err == nil {
 			config.Address = fmt.Sprintf("%s:%s", addr, port)
 		}
